71e48422ed5e2534f7252f200b140935f07b367d
Nikolai Baklanov <nbaklanov@issart.com>
2020-09-01 07:52:26 +0600

    Первое задание по iOS. Начало.

---
+++ NavigationApp/NavigationApp/ContentView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection) {
+            Text("Intro.\nDeveloper Diary.")
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+            Text("Second View")
+            .font(.title)
+            .tabItem {
+                VStack {
+                    Image("second")
+                    Text("Second")
+                }
+            }
+            .tag(2)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
