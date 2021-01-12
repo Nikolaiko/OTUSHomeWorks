39bd9aeb4c73558dffa04f905e34623557bae1ed
Nikolai Baklanov <nbaklanov@issart.com>
2021-01-13 00:25:48 +0600

    DI.

--- DIHomework/Shared/DIHomeworkApp.swift
+++ DIHomework/Shared/DIHomeworkApp.swift
@@ -1,10 +1,14 @@
 import SwiftUI
+import CoreLayerService
 
 @main
 struct DIHomeworkApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    ServiceProvider.shared.addDependancy(dependancy: BreakingBadApi() as NetworkService)
+                }
         }
     }
 }
